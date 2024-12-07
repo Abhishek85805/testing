b442607b2d46aca8e9edb267aef7e80a3c38e760
Abhishek-00000 <abhishekchauhan78763@gmail.com>
2024-12-07 21:55:00 +0530

    port changed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = 443;
 
 app.get('/', (req, res)=>{
     res.send("Hello om mittal")
 })
 
 app.listen(port, ()=>{
-    console.log(`Listring on port ${port}`)
+    console.log(`Listeing on port ${port}`)
 });
