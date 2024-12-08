bada28cd35521fa882cf3ee0dac20fdc9aa67785
Abhishek-00000 <abhishekchauhan78763@gmail.com>
2024-12-08 14:26:08 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 
 const app = express();
-const port = 8080;
+const port = 80;
 
 app.get('/', (req, res)=>{
-    res.send("Hello om mittal")
+    res.send("Kidda fer")
 })
 
 app.listen(port, ()=>{
